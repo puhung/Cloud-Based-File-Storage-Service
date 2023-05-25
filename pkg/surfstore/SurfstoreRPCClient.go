f2402b963204c0da613889854568359f0e7c6d90
puhung <twenet1212@gmail.com>
2023-05-25 15:04:37 -0700

    add SurfstoreRPCClient.go

--- pkg/surfstore/SurfstoreRPCClient.go
+++ pkg/surfstore/SurfstoreRPCClient.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	grpc "google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type RPCClient struct {
@@ -37,23 +38,125 @@ func (surfClient *RPCClient) GetBlock(blockHash string, blockStoreAddr string, b
 }
 
 func (surfClient *RPCClient) PutBlock(block *Block, blockStoreAddr string, succ *bool) error {
-	panic("todo")
+	// connect to the server
+	conn, err := grpc.Dial(blockStoreAddr, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+	c := NewBlockStoreClient(conn)
+
+	// perform the call
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	s, err := c.PutBlock(ctx, &Block{
+		BlockData: block.BlockData,
+		BlockSize: block.BlockSize,
+	})
+	if err != nil {
+		conn.Close()
+		return err
+	}
+	*succ = s.Flag
+
+	// close the connection
+	return conn.Close()
 }
 
 func (surfClient *RPCClient) HasBlocks(blockHashesIn []string, blockStoreAddr string, blockHashesOut *[]string) error {
-	panic("todo")
+	// connect to the server
+	conn, err := grpc.Dial(blockStoreAddr, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+	c := NewBlockStoreClient(conn)
+
+	// perform the call
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	h, err := c.HasBlocks(ctx, &BlockHashes{Hashes: blockHashesIn})
+	if err != nil {
+		conn.Close()
+		return err
+	}
+	*blockHashesOut = h.Hashes
+
+	// close the connection
+	return conn.Close()
 }
 
 func (surfClient *RPCClient) GetFileInfoMap(serverFileInfoMap *map[string]*FileMetaData) error {
-	panic("todo")
+	// connect to the server
+	conn, err := grpc.Dial(surfClient.MetaStoreAddr, grpc.WithInsecure()) // GetFileInfoMap is method of MetaStore (check proto), so we need to dial to MetaStore
+	if err != nil {
+		return err
+	}
+	c := NewMetaStoreClient(conn)
+
+	// perform the call
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	f, err := c.GetFileInfoMap(ctx, &emptypb.Empty{})
+	if err != nil {
+		conn.Close()
+		return err
+	}
+
+	*serverFileInfoMap = f.FileInfoMap
+
+	// close the connection
+	return conn.Close()
 }
 
 func (surfClient *RPCClient) UpdateFile(fileMetaData *FileMetaData, latestVersion *int32) error {
-	panic("todo")
+	// connect to the server
+	conn, err := grpc.Dial(surfClient.MetaStoreAddr, grpc.WithInsecure()) // UpdateFile is method of MetaStore (check proto), so we need to dial to MetaStore
+	if err != nil {
+		return err
+	}
+	c := NewMetaStoreClient(conn)
+
+	// perform the call
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	v, err := c.UpdateFile(ctx, &FileMetaData{
+		Filename:      fileMetaData.Filename,
+		Version:       fileMetaData.Version,
+		BlockHashList: fileMetaData.BlockHashList,
+	})
+	if err != nil {
+		conn.Close()
+		return err
+	}
+
+	*latestVersion = v.Version
+
+	// close the connection
+	return conn.Close()
 }
 
-func (surfClient *RPCClient) GetBlockStoreAddr(blockStoreAddr *string) error {
-	panic("todo")
+func (surfClient *RPCClient) GetBlockStoreAddr(blockStoreAddr *string) error { //blockStoreAddr is a pointer
+	// connect to the server
+	conn, err := grpc.Dial(surfClient.MetaStoreAddr, grpc.WithInsecure()) // GetBlockStoreAddr is method of MetaStore (check proto), so we need to dial to MetaStore
+	if err != nil {
+		return err
+	}
+	c := NewMetaStoreClient(conn)
+
+	// perform the call
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	addr, err := c.GetBlockStoreAddr(ctx, &emptypb.Empty{})
+	if err != nil {
+		conn.Close()
+		return err
+	}
+	// The * is used to "dereference the blockStoreAddr pointer".
+	// This means that the code assigns the value of addr.Addr (which is a string) to the memory location pointed to by blockStoreAddr.
+	// Without the *, it would attempt to assign the value of addr.Addr directly to the pointer itself, which would result in a compilation error.
+	*blockStoreAddr = addr.Addr //update the value of the string that blockStoreAddr points to, allowing the caller of the GetBlockStoreAddr method to receive the new value assigned within the method.
+
+	// close the connection
+	return conn.Close()
 }
 
 // This line guarantees all method for RPCClient are implemented
