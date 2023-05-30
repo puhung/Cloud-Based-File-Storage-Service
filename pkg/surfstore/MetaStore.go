7b87fa8865acde08a267268946ac164f55f00469
puhung <twenet1212@gmail.com>
2023-05-30 10:01:41 -0700

    update MetaStore.go UpdateFile func

--- pkg/surfstore/MetaStore.go
+++ pkg/surfstore/MetaStore.go
@@ -2,9 +2,12 @@ package surfstore
 
 import (
 	context "context"
+	"sync"
 
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
+
+var MetaStoreLock sync.Mutex
 
 type MetaStore struct {
 	FileMetaMap                  map[string]*FileMetaData
@@ -13,19 +16,59 @@ type MetaStore struct {
 }
 
 func (m *MetaStore) GetFileInfoMap(ctx context.Context, _ *emptypb.Empty) (*FileInfoMap, error) {
+	MetaStoreLock.Lock()
+	defer MetaStoreLock.Unlock()
 	return &FileInfoMap{FileInfoMap: m.FileMetaMap}, nil
 }
 
 func (m *MetaStore) UpdateFile(ctx context.Context, fileMetaData *FileMetaData) (*Version, error) {
-	if fileMetaData.Version-m.FileMetaMap[fileMetaData.Filename].Version == 1 {
-		m.FileMetaMap[fileMetaData.Filename] = fileMetaData
-		return &Version{Version: fileMetaData.Version}, nil
+	fileName := fileMetaData.Filename
+	if MetaStoreFileMetaData, exist := m.FileMetaMap[fileName]; exist {
+		if fileMetaData.Version-MetaStoreFileMetaData.Version == 1 {
+			m.FileMetaMap[fileName] = &FileMetaData{
+				Filename:      fileName,
+				Version:       fileMetaData.Version,
+				BlockHashList: fileMetaData.BlockHashList,
+			}
+			return &Version{Version: fileMetaData.Version + 1}, nil
+		} else if fileMetaData.Version == MetaStoreFileMetaData.Version {
+			var same bool
+			same = true
+			if len(fileMetaData.BlockHashList) != len(MetaStoreFileMetaData.BlockHashList) {
+				same = false
+			} else {
+				for i := 0; i < len(fileMetaData.BlockHashList); i++ {
+					if fileMetaData.BlockHashList[i] != MetaStoreFileMetaData.BlockHashList[i] {
+						same = false
+						break
+					}
+				}
+			}
+
+			if same {
+				return &Version{Version: fileMetaData.Version}, nil
+			} else {
+				return &Version{Version: -1}, nil
+			}
+		}
 	} else {
-		return &Version{Version: -1}, nil
+		if fileMetaData.Version == 1 {
+			m.FileMetaMap[fileName] = &FileMetaData{
+				Filename:      fileName,
+				Version:       fileMetaData.Version,
+				BlockHashList: fileMetaData.BlockHashList,
+			}
+			return &Version{Version: fileMetaData.Version}, nil
+		} else {
+			return &Version{Version: -1}, nil
+		}
 	}
+	return &Version{Version: -1}, nil
 }
 
 func (m *MetaStore) GetBlockStoreAddr(ctx context.Context, _ *emptypb.Empty) (*BlockStoreAddr, error) {
+	MetaStoreLock.Lock()
+	defer MetaStoreLock.Unlock()
 	return &BlockStoreAddr{Addr: m.BlockStoreAddr}, nil
 }
 
@@ -37,4 +80,8 @@ func NewMetaStore(blockStoreAddr string) *MetaStore {
 		FileMetaMap:    map[string]*FileMetaData{},
 		BlockStoreAddr: blockStoreAddr,
 	}
+}
+
+func checkHash(fileMetaDataBlockHashList []string, MetaStoreFileMetaDataBlockHashList []string) bool {
+
 }
