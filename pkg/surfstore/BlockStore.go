534ca659b322b5e776a3c6548c35a28a6d02da59
puhung <twenet1212@gmail.com>
2023-05-30 10:04:43 -0700

    modify pkg/surfstore/BlockStore.go

--- pkg/surfstore/BlockStore.go
+++ pkg/surfstore/BlockStore.go
@@ -2,6 +2,8 @@ package surfstore
 
 import (
 	context "context"
+	"errors"
+	"sync"
 )
 
 type BlockStore struct {
@@ -9,18 +11,43 @@ type BlockStore struct {
 	UnimplementedBlockStoreServer
 }
 
+var BlockStoreLock sync.Mutex
+
 func (bs *BlockStore) GetBlock(ctx context.Context, blockHash *BlockHash) (*Block, error) {
-	panic("todo")
+	BlockStoreLock.Lock()
+	defer BlockStoreLock.Unlock()
+
+	block, exists := bs.BlockMap[blockHash.Hash] // block is the block we found, exists is the boolean value
+	if exists {
+		return block, nil
+	} else {
+		return nil, errors.New("Failed to get Block")
+	}
+
 }
 
 func (bs *BlockStore) PutBlock(ctx context.Context, block *Block) (*Success, error) {
-	panic("todo")
+	BlockStoreLock.Lock()
+	defer BlockStoreLock.Unlock()
+	blockHash := GetBlockHashString(block.BlockData)
+	bs.BlockMap[blockHash] = block
+	return &Success{Flag: true}, nil
 }
 
 // Given a list of hashes “in”, returns a list containing the
 // subset of in that are stored in the key-value store
 func (bs *BlockStore) HasBlocks(ctx context.Context, blockHashesIn *BlockHashes) (*BlockHashes, error) {
-	panic("todo")
+	BlockStoreLock.Lock()
+	defer BlockStoreLock.Unlock()
+
+	blockHashes := &BlockHashes{Hashes: []string{}}
+	for _, blockHash := range blockHashesIn.Hashes {
+		_, exists := bs.BlockMap[blockHash]
+		if exists {
+			blockHashes.Hashes = append(blockHashes.Hashes, blockHash)
+		}
+	}
+	return blockHashes, nil
 }
 
 // This line guarantees all method for BlockStore are implemented
